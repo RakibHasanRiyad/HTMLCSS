7887a30188d2ea3184dd0815eee0c1e0e9b39a9c
Md. Rakib Hasan <RakibHasanRiyad@users.noreply.github.com>
2024-10-25 08:15:36 +0600

    Add files via upload

---
+++ 02_heading.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+for ($x = 1; $x <= 6; $x++) {
+    echo "<b><em>The number is: $x </b></em><br>";
+    echo "<em><h$x>This is h$x</h$x></em>";
+	}
+
+for ($x=1; $x<=20; $x++)
+{
+	echo "<p style=\"background-color:lightgreen; color:red;\">this is a paragraph tag. </p>";
+}
+
+
+?>  
+
+</body>
+</html>
